c6ea9237105a3f564790330e0c50f4ab46293efa
arnab sarkar <sarkararnab91@gmail.com>
2025-01-24 18:25:22 +0530

    Object oriented & Functional programming in Python

---
+++ Day-0/main.py
@@ -0,0 +1,12 @@
+import oop
+#from oop import FindNumber as fn
+
+def main():
+    oop.FindNumber().minimum(0,0,0,1,3)
+    oop.FindNumber().palindrome(123321)
+    #oop.FindNumber().div_by_four_sum()
+    oop.DigitOperate().armstrong_number(153)
+    #oop.DigitOperate().div_by_four_sum()
+if __name__ == "__main__":
+    main()
+
--- Day-0/oop.py
+++ Day-0/oop.py
@@ -1,3 +1,56 @@
-class python_learning (x):
+from functools import reduce
+
+class FindNumber():
+    def __init__(self):
+        self.minim = 0
+        self.sum=0
+    def _min_n(self,x,y):
+        return x if x <= y else y
+    def minimum(self,*args):
+        alist = list(map(int, args))
+        #print(alist)
+        # Use filter to filter out smallest number
+        asmall_list = reduce(self._min_n, alist)
+        print("smallest numbers", asmall_list)
+        return asmall_list
     
-    return x
+    def palindrome(self,n):
+        word = str(n)
+        print(word)
+        return print("yes") if word == word[::-1] else print("no")
+    
+    def _div_by_four(self,n):
+        if n%4==0:
+            self.sum+=n
+            return True
+    def div_by_four_sum(self):
+        while True:
+            user_input = input("Enter a number: ")
+            if user_input == "exit":
+                break
+            try:
+                n = int(user_input)
+                if self._div_by_four(n):
+                    print("Sum of numbers divisible by 4: ", self.sum)
+                else:
+                    print("Not divisible by 4")
+            except ValueError:
+                print("Enter a valid number")
+                continue
+
+
+class DigitOperate(FindNumber):
+
+    def __init__(self):
+        pass
+
+    def armstrong_number(self,n:int):
+        int_n_list = map(int,list(str(n)))
+        cube_power_sum = True if reduce(lambda x,y: x+y, map(lambda x: x**3, int_n_list)) == n else False
+        if cube_power_sum:
+            print("Armstrong number")
+        else:
+            print("Not an Armstrong number")
+        
+
+
